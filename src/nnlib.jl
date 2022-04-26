7103d5a519da93fd8a209f639aaad7eac8cbc1ca
Kyle Daruwalla <daruwalla@wisc.edu>
2022-04-26 09:20:36 -0500

    Remove _mask for bitmask

--- src/nnlib.jl
+++ src/nnlib.jl
@@ -1,6 +1,3 @@
-_mask(x::MaskedArray) = x.mask
-_mask(x::MaskedSliceArray) = MaskedArrays._slicetomask(x, x.slices)
-
 for fconv in (:conv, :depthwiseconv), WT in (MaskedArray, MaskedSliceArray)
     local ∇conv_data, ∇conv_filter = Symbol.(:∇, fconv, [:_data, :_filter])
     local conv_pullback, ∇conv_data_pullback = Symbol.([fconv, ∇conv_data], :_pullback)
@@ -20,7 +17,7 @@ for fconv in (:conv, :depthwiseconv), WT in (MaskedArray, MaskedSliceArray)
             y, pb = ChainRulesCore.rrule($fconv, x, freeze(w), cdims; kwargs...)
             function $conv_pullback(Δ)
                 Δs = pb(Δ)
-                return (Δs[1], Δs[2], _mask(w) .* Δs[3], Δs[4])
+                return (Δs[1], Δs[2], MaskedArrays.bitmask(w) .* Δs[3], Δs[4])
             end
             return y, $conv_pullback
         end
@@ -29,7 +26,7 @@ for fconv in (:conv, :depthwiseconv), WT in (MaskedArray, MaskedSliceArray)
             y, pb = ChainRulesCore.rrule($∇conv_data, x, freeze(w), cdims; kwargs...)
             function $∇conv_data_pullback(Δ)
                 Δs = pb(Δ)
-                return (Δs[1], Δs[2], _mask(w) .* Δs[3], Δs[4])
+                return (Δs[1], Δs[2], MaskedArrays.bitmask(w) .* Δs[3], Δs[4])
             end
             return y, $∇conv_data_pullback
         end
