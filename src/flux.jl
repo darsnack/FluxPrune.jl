233b979abc3830e297f4aba0f0ab36c0da6762e8
Kyle Daruwalla <daruwalla@wisc.edu>
2022-04-26 10:45:54 -0500

    Update data not x

--- src/flux.jl
+++ src/flux.jl
@@ -1,6 +1,6 @@
 function Flux.Optimise.update!(opt::Flux.Optimise.AbstractOptimiser, x::T, dx) where T<:Union{<:MaskedArray, <:MaskedSliceArray}
     Flux.Optimise.update!(opt, freeze(x), dx)
-    x .-= dx
+    x.data .-= dx
 
     return x
 end
