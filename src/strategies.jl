a2050f0062116015e016061f7fe12deee7e87b8c
Kyle Daruwalla <daruwalla@wisc.edu>
2022-04-27 15:31:05 -0500

    Don't keep iterating if pruning fails

--- src/strategies.jl
+++ src/strategies.jl
@@ -8,6 +8,16 @@ function iterativeprune(finetune!, stages, m; maxtries = 5)
         while (attempts < maxtries) && !finetune!(m̄)
             attempts += 1
         end
+
+        # exit early if we failed
+        if attempts >= maxtries
+            @warn """
+            Failed to finetune model to target, exiting pruning early.
+            Returning finetuned model from last successful stage.
+            """
+            break
+        end
+
         m = m̄
     end
 
